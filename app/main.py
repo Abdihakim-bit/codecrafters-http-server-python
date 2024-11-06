bfdeb8bad45612088255f83ec30fcbe94181cac5
abdyh <aali3585@conestogac.on.ca>
2024-11-06 03:44:27 -0500

    5th stage - Read Header: Placed the response endpoints in one function and added the /user-agent endpoint

--- app/main.py
+++ app/main.py
@@ -1,29 +1,35 @@
 import socket  # noqa: F401
 import re
 
+CRLF = "\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221))
     serveOn = server_socket.accept() # wait for client and store their details
-    CRLF = "\r\n"
     if serveOn is not None:
         request = serveOn[0].recv(1024).decode()
-        requestParts = request.split(CRLF)
-        if (" / " in requestParts[0]):
-            response = "HTTP/1.1 200 OK"+(CRLF*2)
-        elif(" /echo/" in requestParts[0]):
-            echoString = requestParts[0].split("/echo/")
-            if (echoString[1] is not None):
-                text = echoString[1].split(" ")
-                response = "HTTP/1.1 200 OK"+CRLF+"Content-Type: text/plain"+CRLF+"Content-Length: "+str(len(text[0]))+""+(CRLF*2)+text[0]
-        else:
-            response = "HTTP/1.1 404 Not Found"+(CRLF*2)
-        serveOn[0].send(response.encode())
+        serveOn[0].send(response(request).encode())
 
+    # Parses the user request and returns appropriate response    
+def response(request):
+    requestParts = request.split(CRLF)
+    if (" / " in requestParts[0]):
+        response = "HTTP/1.1 200 OK"+(CRLF*2)
+    elif(" /echo/" in requestParts[0]):
+        echoString = requestParts[0].split("/echo/")
+        if (echoString[1] is not None):
+            text = echoString[1].split(" ")
+            response = "HTTP/1.1 200 OK"+CRLF+"Content-Type: text/plain"+CRLF+"Content-Length: "+str(len(text[0]))+""+(CRLF*2)+text[0]
+    elif(" /user-agent" in requestParts[0]):
+        userAgent = requestParts[3].split(": ")
+        if (userAgent[1] is not None):
+            response = "HTTP/1.1 200 OK"+CRLF+"Content-Type: text/plain"+CRLF+"Content-Length: "+str(len(userAgent[1]))+""+(CRLF*2)+userAgent[1]
+    else:
+        response = "HTTP/1.1 404 Not Found"+(CRLF*2)
+    return response
 if __name__ == "__main__":
     main()
