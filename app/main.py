1cca34544e3a0faa80d9dee5a830d3d2454dd6bb
abdyh <aali3585@conestogac.on.ca>
2024-11-06 00:16:49 -0500

    Removed reuse_port=True

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221))
     server_socket.accept() # wait for client
 
 
