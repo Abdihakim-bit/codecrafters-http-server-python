8d0a6904e150c00085a913d83684fca7df424a98
abdyh <aali3585@conestogac.on.ca>
2024-11-06 23:42:03 -0500

    6th stage - Concurrent connection: Added a while loop to keep the server available for connection until Keyboard Interrup is registered

--- app/main.py
+++ app/main.py
@@ -2,37 +2,47 @@ import socket  # noqa: F401
 import re
 
 CRLF = "\r\n"
+response200 = "HTTP/1.1 200 OK"
+response404 = "HTTP/1.1 404 Not Found"
+contentTypeTxt = "Content-Type: text/plain"
+contentLength = "Content-Length: "
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
+    # Create a socket server on the local machine to listen on port 4221.
     server_socket = socket.create_server(("localhost", 4221))
-    serveOn = server_socket.accept() # wait for client and store their details
-    if serveOn is not None:
-        request = serveOn[0].recv(1024).decode()
-        serveOn[0].send(response(request).encode())
+    try:
+        while True:
+            serveOn = server_socket.accept() # wait for client and store their details
+            if serveOn is not None:
+                request = serveOn[0].recv(1024).decode()
+                serveOn[0].send(response(request).encode())    
+    except KeyboardInterrupt:
+        server_socket.close()
+        print("Shutting down server")
 
+        
     # Parses the user request and returns appropriate response    
 def response(request):
     requestParts = request.split(CRLF)
     if (" / " in requestParts[0]):
-        response = "HTTP/1.1 200 OK"+(CRLF*2)
+        response = response200+(CRLF*2)
     elif(" /echo/" in requestParts[0]):
         echoString = requestParts[0].split("/echo/")
         if (echoString[1] is not None):
             text = echoString[1].split(" ")
-            response = "HTTP/1.1 200 OK"+CRLF+"Content-Type: text/plain"+CRLF+"Content-Length: "+str(len(text[0]))+""+(CRLF*2)+text[0]
+            response = response200+CRLF+contentTypeTxt+CRLF+contentLength+str(len(text[0]))+""+(CRLF*2)+text[0]
     elif(" /user-agent" in requestParts[0]):
-        userAgent = requestParts[2].split(": ")
+        userAgent = requestParts[2].split(": ") # Split the third line using ":" and store the results in userAgent
         if ("User-Agent" not in userAgent[0]):
-            userAgent = requestParts[3].split(": ")
+            userAgent = requestParts[3].split(": ") # Split the fourth line instead if "User-Agent" is not in userAgent
         if (userAgent[1] is not None):
             print("", userAgent[1])
-            response = "HTTP/1.1 200 OK"+CRLF+"Content-Type: text/plain"+CRLF+"Content-Length: "+str(len(userAgent[1]))+""+(CRLF*2)+userAgent[1]
+            response = response200+CRLF+contentTypeTxt+CRLF+contentLength+str(len(userAgent[1]))+""+(CRLF*2)+userAgent[1]
     else:
-        response = "HTTP/1.1 404 Not Found"+(CRLF*2)
+        response = response404+(CRLF*2)
     return response
 if __name__ == "__main__":
     main()
