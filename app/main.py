ece0868963d6532a82f62bf8ce07c20cfdbb3934
abdyh <aali3585@conestogac.on.ca>
2024-11-12 12:07:01 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     try:
-        base_directory = sys.argv[1]
+        base_directory = sys.argv[2]
     except IndexError:
         base_directory = str(pathlib.Path().absolute())
     start_server(base_directory)
